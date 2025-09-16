542d9a80c6d3095d02173d581c58899caad881db
Gosesummer <51499066+suneao@users.noreply.github.com>
2025-09-16 17:39:12 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Fuck you</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://minecraft.net',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://bilibili',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'I want to fuck this temple',
   },
 }
 
