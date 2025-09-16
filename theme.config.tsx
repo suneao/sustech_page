a4edbc1612253229394706236ead67db71594707
Gosesummer <2953863750@qq.com>
2025-09-16 18:16:36 +0800

    basic setup

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Fuck you</span>,
+  logo: <span>南科大资源页</span>,
   project: {
     link: 'https://github.com/suneao/sustech_page',
   },
@@ -12,6 +12,11 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/suneao/sustech_page',
   footer: {
     text: 'I want to fuck this temple',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s'
+    }
   },
 }
 
